d6fa6e4c0fd20c60669620b3ee3a93ebd30c0ef8
Howard Post <hpost2012@gmail.com>
2020-06-22 08:31:53 -0700

    first attempt at code.  Changed it so my twitter keys would not upload to github.

--- retrievetrainingdata.py
+++ retrievetrainingdata.py
@@ -1,44 +1,72 @@
 import twitter
+import practicecode.keys as key
 
 
 # initialize api instance
-twitter_api = twitter.Api(consumer_key='YHe4bNHnfbmlmGadqPwbc5Srx',
-                        consumer_secret='yph8kAuAUSUr3A3ZMYz9ePoLNrumrRsuySipzVAv6J9iwxptYF',
-                        access_token_key='1186688700672905216-gzDSMDolafPu9D06aiXsQ2zEFDMNRe',
-                        access_token_secret='PXRuI3uQTRRmTdGlBdhWRplqAk8aXZXDjr1lsV5GMnGuT')
+twitter_api = twitter.Api(consumer_key=key.consumer_key,
+                          consumer_secret=key.consumer_secret,
+                          access_token_key=key.access_key,
+                          access_token_secret=key.access_secret)
 
 print(twitter_api.VerifyCredentials())
+
+
 def buidTrainingSet(corpusFile, tweetDataFile):
     import csv
     import time
-    
+
     corpus = []
-    
-    with open(corpusFile,'r') as csvfile:
-        lineReader = csv.reader(csvfile,delimiter=',', quotechar="\"")
+
+    with open(corpusFile, 'r') as csvfile:
+        lineReader = csv.reader(csvfile, delimiter=',', quotechar="\"")
         for row in lineReader:
-            corpus.append({"tweet_id":row[2], "label":row[1], "topic":row[0]})
-            
+            corpus.append({"tweet_id": row[2],
+                           "label": row[1],
+                           "topic": row[0]})
+
     rate_limit = 180
     sleep_time = 900/180
-    
+
     trainingDataSet = []
-    
+    rate_counter = 0
     for tweet in corpus:
         try:
-            status = twitter_api.GetStatus(tweet["tweet_id"])
-            print("Tweet fetched" + status.text)
-            tweet["text"] = status.text
-            trainingDataSet.append(tweet)
-            time.sleep(sleep_time) 
-        except: 
+            if rate_counter < rate_limit:
+                status = twitter_api.GetStatus(tweet["tweet_id"])
+                print("Tweet fetched" + status.text)
+                tweet["text"] = status.text
+                trainingDataSet.append(tweet)
+                rate_counter += 1
+            else:
+                time.sleep(sleep_time)
+        except EnvironmentError as e:
+            print(e)
             continue
     # now we write them to the empty CSV file
-    with open(tweetDataFile,'w') as csvfile:
-        linewriter = csv.writer(csvfile,delimiter=',',quotechar="\"")
+    with open(tweetDataFile, 'w') as csvfile:
+        linewriter = csv.writer(csvfile, delimiter=',', quotechar="\"")
         for tweet in trainingDataSet:
             try:
-                linewriter.writerow([tweet["tweet_id"], tweet["text"], tweet["label"], tweet["topic"]])
+                linewriter.writerow([tweet["tweet_id"],
+                                    tweet["text"],
+                                    tweet["label"],
+                                    tweet["topic"]])
             except Exception as e:
                 print(e)
     return trainingDataSet
+
+
+def load_training_data(file):
+    import csv
+    tweets = []
+    with open(file) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            temp_dict = {'text': row[1], 'label': row[2]}
+            tweets.append(temp_dict.copy())
+        # for line in f:
+        #     tweet_data = line.split(',')
+        #     print(len(tweet_data))
+        #     temp_dict = {'text': tweet_data[1], 'label': tweet_data[2]}
+        #     tweets.append(temp_dict.copy())
+    return tweets
--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -1,16 +1,96 @@
 import sys
 import re
-from retrievetrainingdata import buidTrainingSet as build_training
+# from retrievetrainingdata import buidTrainingSet as build_training
+from retrievetrainingdata import load_training_data
+from gathertweets import get_tweets
+from nltk.tokenize import word_tokenize
+from string import punctuation
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+
+word_features = []
 
 
-def create_parser():
-    pass
+class PreProcessTweets:
+    def __init__(self):
+        self._stopwords = set(stopwords.words('english')
+                              + list(punctuation)
+                              + ['AT_USER', 'URL'])
+
+    def process_tweets(self, tweets_list):
+        processed_tweets = []
+        for tweet in tweets_list:
+            processed_tweets.append((self._process_tweet(tweet["text"]),
+                                    tweet["label"]))
+        return processed_tweets
+
+    def _process_tweet(self, tweet):
+        tweet = tweet.lower()
+        tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+        tweet = re.sub(r'@[^\s]+', 'AT_USER', tweet)
+        tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+        tweet = word_tokenize(tweet)
+        return [word for word in tweet if word not in self._stopwords]
 
 
-def main(args):
-    training_data = build_training("corpus.csv", "tweetDataFile.csv")
+def build_vocabulary(preprocessed_data):
+    all_words = []
+    for (words, sentiment) in preprocessed_data:
+        all_words.extend(words)
+    words_list = nltk.FreqDist(all_words)
+    word_features = words_list.keys()
+    return word_features
+
+
+def extract_features(tweet):
+    tweet_words = set(tweet)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in tweet_words)
+    return features
+
+
+def main():
+    tweet_processor = PreProcessTweets()
+    global word_features
+    search_list = []
+    search_term = input("Enter the topic you want to search for: ")
+    search_list.append(search_term)
+    # print(search_list)
+    # training_data = build_training("corpus.csv", "tweetDataFile.csv")
+    training_tweets = load_training_data("tweetDataFile.csv")
+    test_tweets = get_tweets(search_list)
+    preproc_training_tweets = tweet_processor.process_tweets(
+                                                            training_tweets)
+    preprocessed_test_tweets = tweet_processor.process_tweets(test_tweets)
+    word_features = build_vocabulary(preproc_training_tweets)
+    training_features = nltk.classify.apply_features(extract_features,
+                                                     preproc_training_tweets)
+    NBayesClassifier = nltk.NaiveBayesClassifier.train(training_features)
+    NBayesClassifier.show_most_informative_features(15)
+    extracted_test_tweets = nltk.classify.apply_features(extract_features,
+                                                         preprocessed_test_tweets)
+    
+    print("NBayes Classifier accuracy percent: ",
+          nltk.classify.accuracy(NBayesClassifier, extracted_test_tweets))
+    NBResultLabels = [NBayesClassifier.classify(extract_features(tweet[0]))
+                      for tweet in preprocessed_test_tweets]
+
+    if NBResultLabels.count('positive') > NBResultLabels.count('negative'):
+        print("Overall Positive Sentiment")
+        print("Positive Sentiment Percentage = "
+              + str(100*NBResultLabels.count('positive')/len(NBResultLabels))
+              + "%")
+    else:
+        print("Overall Negative Sentiment")
+        print("Negative Sentiment Percentage = "
+              + str(100*NBResultLabels.count('negative')/len(NBResultLabels))
+              + "%")
+    # print(test_tweets)
     pass
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
